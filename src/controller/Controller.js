8baee3fb0b2d346180ce5745e0b288ef096ed557
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2025-02-17 00:55:43 -0300

    criando o crud completo

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,13 +1,48 @@
-class Constroller {
+class Controller {
   constructor(entityService) {
     this.entityService = entityService;
   }
   async getAll(req, res) {
     try {
-      const listOfRegisters = await this.entityService.getAllRegisters();
-      return res.status(200).json(listOfRegisters);
+      const listOfRecord = await this.entityService.getAllRecords();
+      return res.status(200).json(listOfRecord);
+    } catch (error) {}
+  }
+  async takeOneById(req, res) {
+    const { id } = req.params;
+    try {
+      const oneRecord = await this.entityService.getRecordById(Number(id));
+      return res.status(200).json(oneRecord);
+    } catch (error) {}
+  }
+  async createNew(req, res) {
+    try {
+      const dataForCreate = await this.entityService.createRecord();
+      return res.status(200).json(dataForCreate);
+    } catch (error) {}
+  }
+  async uptade(req, res) {
+    const { id } = req.params;
+    const dataUdated = req.body;
+    try {
+      const isUpdated = await this.entityService.updateRecord(
+        dataUdated,
+        Number(id)
+      );
+      if (!isUpdated) {
+        return res.status(400).json({ mensagem: "Register not found" });
+      }
+      return res.status(200).json({ mensagem: "updated successfully" });
+    } catch (error) {}
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+    try {
+      await this.entityService.deleteRecord(Number(id));
+      return res.status(200).json({ mensagem: `id ${id} deleted` });
     } catch (error) {}
   }
 }
 
-module.exports = Constroller;
+module.exports = Controller;
--- src/services/Services.js
+++ src/services/Services.js
@@ -1,11 +1,35 @@
+const { where } = require("sequelize");
 const dataSouce = require("../models");
 
 class Services {
   constructor(nameOfModel) {
     this.model = nameOfModel;
   }
-  async getAllRegisters() {
+  async getAllRecords() {
     return dataSouce[this.model].findAll();
+  }
+  async getRecordById(id) {
+    return dataSouce[this.model].findByPk(id);
+  }
+  async createRecord(dataOfRecord) {
+    return dataSouce[this.model].create(dataOfRecord);
+  }
+
+  async updateRecord(dataUptade, id) {
+    const listOfRecordsUptaed = dataSouce[this.model].update(dataUptade, {
+      where: { id: id },
+    });
+    if (listOfRecordsUptaed[0] === 0) {
+      return false;
+    }
+    return true;
+  }
+  async deleteRecord(id) {
+    return dataSouce[this.model].destroy({
+      where: {
+        id: id,
+      },
+    });
   }
 }
 
